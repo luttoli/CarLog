35fbadb4c3331e9aa55991531ab4b06e9f7f97b3
luttoli <hhn0212@naver.com>
2022-07-29 18:07:04 +0900

    git 설정

---
+++ CarLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarLog
+//
+//  Created by 김지훈 on 2022/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
