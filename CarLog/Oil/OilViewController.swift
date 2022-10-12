d95e832461a08cfbcc1e735ae5ae1c2ef805580b
김지훈 <hhn0212@naver.com>
2022-10-13 02:21:46 +0900

    스토리보드 수정

--- CarLog/Oil/OilViewController.swift
+++ CarLog/Oil/OilViewController.swift
@@ -146,7 +146,7 @@ extension OilViewController: UICollectionViewDataSource {
 
 extension OilViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (UIScreen.main.bounds.width) - 20, height: 90)
+        return CGSize(width: (UIScreen.main.bounds.width) - 20, height: 72)
     }
 }
 
